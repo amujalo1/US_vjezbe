170fa4eb276ec8f004b848306f643f3cfabd0277
amujalo1 <amujalo1@etf.unsa.ba>
2024-04-05 08:13:59 +0200

    prepravka 2 i 3

--- vjezba_3/zadatak 2/main.py
+++ vjezba_3/zadatak 2/main.py
@@ -1,44 +1,32 @@
-import rp2
-from rp2 import PIO
 from machine import Pin
 import time
 
-
-# ------ #
-# sevseg #
-# ------ #
-
-@rp2.asm_pio(out_init=[PIO.OUT_LOW] * 8, sideset_init=[PIO.OUT_LOW] * 4)
-def sevseg():
-    wrap_target()
-    label("0")
-    pull(noblock).side(0)  # 0
-    mov(x, osr).side(0)  # 1
-    out(pins, 8).side(1)  # 2
-    out(pins, 8).side(2)  # 3
-    out(pins, 8).side(4)  # 4
-    out(pins, 8).side(8)  # 5
-    jmp("0").side(0)  # 6
-    wrap()
-
-
-sm = rp2.StateMachine(0, sevseg, freq=2000, out_base=Pin(2), sideset_base=Pin(10))
-sm.active(1)
+segments = [9,2,3,4,5,6,7,8] #DP A B C D E F G
+dig = [10,11,12,13] #4, 3, 2, 1: 4 odgovara za hiljdatidi broj, 3 za stotice 2 za desetice i 1 za jedinice
 
 digits = [
-    0b11000000,  # 0
-    0b11111001,  # 1
-    0b10100100,  # 2
-    0b10110000,  # 3
-    0b10011001,  # 4
-    0b10010010,  # 5
-    0b10000010,  # 6
-    0b11111000,  # 7
-    0b10000000,  # 8
-    0b10011000,  # 9
-    0b11111111,  # prazno
-
+    [0,1,1,1,1,1,1,0], # 0
+    [0,0,1,1,0,0,0,0], # 1
+    [0,1,1,0,1,1,0,1], # 2
+    [0,1,1,1,1,0,0,1], # 3
+    [0,0,1,1,0,0,1,1], # 4
+    [0,1,0,1,1,0,1,1], # 5
+    [0,1,0,1,1,1,1,1], # 6
+    [0,1,1,1,0,0,0,0], # 7
+    [0,1,1,1,1,1,1,1], # 8
+    [0,1,1,1,1,0,1,1], # 9
+    [0,0,0,0,0,0,0,0], # prazno
+    [1,0,0,0,0,0,0,0], # zarez
 ]
+digits = [[not bit for bit in digit] for digit in digits]
+
+def print7segment(digits_array):
+  for c, cifra in enumerate(digits_array):
+    for i, pin in enumerate(segments):
+      Pin(pin,Pin.OUT).value(cifra[i])
+    Pin(dig[c], Pin.OUT).value(1)
+    time.sleep_ms(20)
+    Pin(dig[c], Pin.OUT).value(0)
 
 button1 = Pin(18, Pin.IN, Pin.PULL_UP)
 button2 = Pin(19, Pin.IN, Pin.PULL_UP)
@@ -48,45 +36,47 @@ button4 = Pin(21, Pin.IN, Pin.PULL_UP)
 
 def segmentize(num, vel):
     segs = [
-        digits[num % 10],  # Posljednja cifra
-        digits[num // 10 % 10] << 8,  # Pretposljednja cifra
-        digits[num // 100 % 10] << 16,  # Treća cifra s desna
-        digits[num // 1000 % 10] << 24  # Prva cifra
+        digits[num // 1000 % 10],  # Prva cifra
+        digits[num // 100 % 10],  # Treća cifra s desna
+        digits[num // 10 % 10],  # Pretposljednja cifra
+        digits[num % 10]  # Posljednja cifra
+
     ]
     if num % 10 == 0 and vel == 0:
-        segs[0] = digits[10]
+        segs[3] = digits[10]
     if (num // 10) % 10 == 0 and vel < 2:
-        segs[1] = digits[10] << 8
+        segs[2] = digits[10] 
     if (num // 100) % 10 == 0 and vel < 3:
-        segs[2] = digits[10] << 16
+        segs[1] = digits[10]
     if (num // 1000) % 10 == 0 and vel < 4:
-        segs[3] = digits[10] << 24
+        segs[0] = digits[10]
 
-    return segs[0] | segs[1] | segs[2] | segs[3]
+    return segs
 
 
-# sm.put(segmentize(counter));
 counter = 0
+print7segment([digits[10]]*4);
 auto_counting = False
 
 # Glavna petlja
 while True:
+    print7segment(segmentize(counter,len(str(counter))));
     if not button1.value():  # Provera tastera 1 (uvećavanje)
         counter += 1
-        sm.put(segmentize(counter, len(str(counter))))
-        time.sleep_ms(200)  # Dilej nakon što se pritisne taster
+        print7segment(segmentize(counter, len(str(counter))))
+        time.sleep_ms(100)  # Dilej nakon što se pritisne taster
         while not button1.value():  # Čekanje dok se taster ne otpusti
             time.sleep_ms(20)
     elif not button2.value():  # Provera tastera 2 (smanjivanje)
         counter -= 1
-        sm.put(segmentize(counter, len(str(counter))))
-        time.sleep_ms(200)  # Dilej nakon što se pritisne taster
+        print7segment(segmentize(counter, len(str(counter))))
+        time.sleep_ms(100)  # Dilej nakon što se pritisne taster
         while not button2.value():  # Čekanje dok se taster ne otpusti
             time.sleep_ms(20)
     elif not button3.value():  # Provera tastera 3 (resetovanje)
         counter = 0
-        sm.put(segmentize(counter, len(str(counter))));
-        time.sleep_ms(200)  # Dilej nakon što se pritisne taster
+        print7segment(segmentize(counter, len(str(counter))));
+        time.sleep_ms(100)  # Dilej nakon što se pritisne taster
         while not button3.value():  # Čekanje dok se taster ne otpusti
             time.sleep_ms(20)
     elif not button4.value():  # Provera tastera 4 (pokretanje/zaustavljanje automatskog brojanja)
@@ -95,9 +85,10 @@ while True:
             time.sleep_ms(20)
     if auto_counting:
         counter += 1  # Automatsko inkrementiranje
-        sm.put(segmentize(counter, len(str(counter))))
-        time.sleep_ms(1000)  # Dilej za automatsko brojanje
+        for _ in range(10):
+            print7segment(segmentize(counter, len(str(counter))))
+            time.sleep_ms(10)  # Dilej za automatsko brojanje
+
     else:
         print(counter)
-        sm.put(segmentize(counter, len(str(counter))))
-
+        print7segment(segmentize(counter, len(str(counter))))
--- vjezba_3/zadatak 3 - izbor 2/main.py
+++ vjezba_3/zadatak 3 - izbor 2/main.py
@@ -22,30 +22,34 @@ for row in ROWS:
 
 
 # Funkcija za čitanje pritisnutih tastera
-def read_keys():
+def read_keys(stanje):
     pressed_keys = []
     for col_index, col_pin in enumerate(COLS):
         col_pin.value(1)
+        print7segment(stanje)
         for row_index, row_pin in enumerate(ROWS):
             if row_pin.value() == 1:
+                print7segment(stanje)
                 pressed_keys.append(key_map[row_index][col_index])
-                utime.sleep_ms(200)  # Dodajemo kratki delay kako bismo spriječili višestruka čitanja
+                utime.sleep_ms(100)  # Dodajemo kratki delay kako bismo spriječili višestruka čitanja
         col_pin.value(0)
     return pressed_keys
 
 
 # Funkcija za čitanje pritisnutih tastera, uzima samo brojeve
-def read_numbers():
+def read_numbers(stanje):
     pressed_keys = []
     for col_index, col_pin in enumerate(COLS):
         col_pin.value(1)
+        print7segment(stanje)
         for row_index, row_pin in enumerate(ROWS):
             if row_pin.value() == 1:
+                print7segment(stanje)
                 key_pressed = key_map[row_index][col_index]
                 # Provjeravamo da li je pritisnuti taster broj
                 if key_pressed.isdigit():
                     pressed_keys.append(key_pressed)
-                utime.sleep_ms(200)  # Dodajemo kratki delay kako bismo spriječili višestruka čitanja
+                utime.sleep_ms(10)  # Dodajemo kratki delay kako bismo spriječili višestruka čitanja
         col_pin.value(0)
     return pressed_keys
 
@@ -56,127 +60,127 @@ def wait_for_release():
         pass
 
 
-import rp2
-from rp2 import PIO
 from machine import Pin
 import time
 
-
-# ------ #
-# sevseg #
-# ------ #
-
-@rp2.asm_pio(out_init=[PIO.OUT_LOW] * 8, sideset_init=[PIO.OUT_LOW] * 4)
-def sevseg():
-    wrap_target()
-    label("0")
-    pull(noblock).side(0)  # 0
-    mov(x, osr).side(0)  # 1
-    out(pins, 8).side(1)  # 2
-    out(pins, 8).side(2)  # 3
-    out(pins, 8).side(4)  # 4
-    out(pins, 8).side(8)  # 5
-    jmp("0").side(0)  # 6
-    wrap()
-
-
-sm = rp2.StateMachine(0, sevseg, freq=2000, out_base=Pin(10), sideset_base=Pin(17))
-sm.active(1)
+segments = [21, 10, 11, 12, 13, 14, 15, 16]  # DP A B C D E F G
+dig = [17, 18, 19, 20]  # 4, 3, 2, 1: 4 odgovara za hiljdatidi broj, 3 za stotice 2 za desetice i 1 za jedinice
 
 digits = [
-    0b11000000,  # 0
-    0b11111001,  # 1
-    0b10100100,  # 2
-    0b10110000,  # 3
-    0b10011001,  # 4
-    0b10010010,  # 5
-    0b10000010,  # 6
-    0b11111000,  # 7
-    0b10000000,  # 8
-    0b10011000,  # 9
-    0b11111111,  # prazno
+    [0, 1, 1, 1, 1, 1, 1, 0],  # 0
+    [0, 0, 1, 1, 0, 0, 0, 0],  # 1
+    [0, 1, 1, 0, 1, 1, 0, 1],  # 2
+    [0, 1, 1, 1, 1, 0, 0, 1],  # 3
+    [0, 0, 1, 1, 0, 0, 1, 1],  # 4
+    [0, 1, 0, 1, 1, 0, 1, 1],  # 5
+    [0, 1, 0, 1, 1, 1, 1, 1],  # 6
+    [0, 1, 1, 1, 0, 0, 0, 0],  # 7
+    [0, 1, 1, 1, 1, 1, 1, 1],  # 8
+    [0, 1, 1, 1, 1, 0, 1, 1],  # 9
+    [0, 0, 0, 0, 0, 0, 0, 0],  # prazno
+    [1, 0, 0, 0, 0, 0, 0, 0],  # zarez
+    [0, 0, 0, 0, 0, 0, 0, 1],  # minus
 ]
-DP = Pin(21, Pin.OUT)
+digits = [[not bit for bit in digit] for digit in digits]
+
+
+def print7segment(digits_array):
+    for c, cifra in enumerate(digits_array):
+        for i, pin in enumerate(segments):
+            Pin(pin, Pin.OUT).value(cifra[i])
+        Pin(dig[c], Pin.OUT).value(1)
+        time.sleep_ms(20)
+        Pin(dig[c], Pin.OUT).value(0)
 
 
 def segmentize(num, vel):
     segs = [
-        digits[num % 10],  # Posljednja cifra
-        digits[num // 10 % 10] << 8,  # Pretposljednja cifra
-        digits[num // 100 % 10] << 16,  # Treća cifra s desna
-        digits[num // 1000 % 10] << 24  # Prva cifra
+        digits[num // 1000 % 10],  # Prva cifra
+        digits[num // 100 % 10],  # Treća cifra s desna
+        digits[num // 10 % 10],  # Pretposljednja cifra
+        digits[num % 10]  # Posljednja cifra
+
     ]
     if num % 10 == 0 and vel == 0:
-        segs[0] = digits[10]
+        segs[3] = digits[10]
     if (num // 10) % 10 == 0 and vel < 2:
-        segs[1] = digits[10] << 8
+        segs[2] = digits[10]
     if (num // 100) % 10 == 0 and vel < 3:
-        segs[2] = digits[10] << 16
+        segs[1] = digits[10]
     if (num // 1000) % 10 == 0 and vel < 4:
-        segs[3] = digits[10] << 24
+        segs[0] = digits[10]
 
-    return segs[0] | segs[1] | segs[2] | segs[3]
+    return segs
 
 
-def CekanjeZaUnosHesha():
+def CekanjeZaUnosHesha(stanje):
     while True:
-        pressed = read_keys()
+        print7segment(stanje)
+        pressed = read_keys(stanje)
         if pressed and pressed[0] == '#':
             # Ovdje dodajte kod koji se izvršava nakon unosa #
             break
         else:
             wait_for_release()
-        utime.sleep_ms(100)  # Pauza između očitavanja
+        utime.sleep_ms(10)  # Pauza između očitavanja
 
 
 # Glavna petlja
-DP.on()
 waiting_for_pound = False
 num4 = ""
 wrong_attempts = 0  # Brojač pogrešnih pokušaja
-sm.put(segmentize(0, 0))
+print7segment([digits[10]] * 4)
 
 while True:
-    pressed = read_numbers()
+    if num4 == "":
+        pressed = read_numbers([digits[10]] * 4)
+    else:
+        pressed = read_numbers(segmentize(int(num4), len(num4)))
     if pressed:
         num4 += ''.join(pressed)
 
-        sm.put(segmentize(int(num4), len(num4)))
+        print7segment(segmentize(int(num4), len(num4)))
         if len(num4) == 4:
             waiting_for_pound = True
     else:
         if waiting_for_pound:
-            CekanjeZaUnosHesha()
+            CekanjeZaUnosHesha(segmentize(int(num4), len(num4)))
             if num4 == '1234':  # Promijeniti sa stvarnim PIN-om
-                sm.put((digits[10] | digits[10] << 8 | digits[10] << 16 | digits[10] << 24))  # tacan pin
+                # tacan pin
                 for _ in range(5):
-                    DP.off()
-                    utime.sleep_ms(500)
-                    DP.on()
-                    utime.sleep_ms(500)
-                sm.put(segmentize(0, 0))
+                    for _ in range(5):
+                        print7segment([digits[11]] * 4)
+                        utime.sleep_ms(10)
+                    for _ in range(5):
+                        print7segment([digits[10]] * 4)
+                        utime.sleep_ms(10)
+                print7segment([digits[10]] * 4)
             else:
                 wrong_attempts += 1
                 if wrong_attempts == 3:
                     for i in range(9, -1, -1):  # Odbrojavanje od 9 do 0
-                        sm.put(segmentize(i, 1))
-                        utime.sleep(1)
-                    sm.put(segmentize(0, 0))
+                        for _ in range(10):
+                            print7segment(segmentize(i, 1))
+                            utime.sleep_ms(10)
+
+                    print7segment([digits[10]] * 4)
                     wrong_attempts = 0  # Resetujemo brojač pogrešnih pokušaja
                     num4 = ""  # Resetujemo num4 nakon odbrojavanja
                     waiting_for_pound = False
                     continue  # Nastavljamo sa čekanjem unosa PIN-a
-                for _ in range(5):
-                    sm.put((0b10111111 | 0b10111111 << 8 | 0b10111111 << 16 | 0b10111111 << 24))
-                    utime.sleep_ms(500)  # Pogrešan PIN
-                    sm.put((0b11111111 | 0b11111111 << 8 | 0b11111111 << 16 | 0b11111111 << 24))
-                    utime.sleep_ms(500)
-                sm.put(segmentize(0, 0))
+                for _ in range(5):  # Pogrešan PIN
+                    for _ in range(5):
+                        print7segment([digits[12]] * 4)
+                        utime.sleep_ms(10)
+                    for _ in range(5):
+                        print7segment([digits[10]] * 4)
+                        utime.sleep_ms(10)
+                print7segment([digits[10]] * 4)
             num4 = ""  # Resetujemo num4 nakon provjere PIN-a
             waiting_for_pound = False
             wait_for_release()
         else:
             wait_for_release()
             prev_pressed = []
-    utime.sleep_ms(100)  # Pauza između očitavanja
+    utime.sleep_ms(10)  # Pauza između očitavanja
 
